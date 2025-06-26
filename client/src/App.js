6bf515e4f03174229136b82c2fec4e254fd36528
HanadAlly <allyhanad404@gmail.com>
2025-06-26 11:29:57 +0300

    commit changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,25 @@
-client/src/App.js
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import StationList from './components/StationList';
-import StationForm from './components/StationForm';
-import StationDetail from './components/StationDetail';
-import Login from './components/Login';
-import About from './components/About';
-import GetStarted from './components/GetStarted';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import StationList from "./components/StationList";
+import StationForm from "./components/StationForm";
+import About from "./components/About";
+import GetStarted from "./components/GetStarted";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<GetStarted />} />
         <Route path="/find-stations" element={<StationList />} />
         <Route path="/stations/new" element={<StationForm />} />
-        <Route path="/stations/:id" element={<StationDetail />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
         <Route path="/get-started" element={<GetStarted />} />
-        <Route path="/" element={<GetStarted />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
